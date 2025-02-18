fdf23b5b134937766d417f306287512785a7ab90
adrianopfa <adriano.pfa@gmail.com>
2025-02-18 16:48:42 -0300

    Complemetos das atividades

--- Desktop/Lógica de Programação JavaScript/Trabalho/challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ Desktop/Lógica de Programação JavaScript/Trabalho/challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -1,8 +1,40 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-let nomeSecreto = []
+
+let amigo = [];
+
 
 function adicionarAmigo() {
-    let amigoSecreto =  document.querySelector('input').value;
-    nomeSecreto.push(amigoSecreto);
-    console.log(nomeSecreto);
+    let amigoSecreto =  document.querySelector('#amigo').value;
+    
+    if (amigoSecreto.trim() !==""){
+    amigo.push(amigoSecreto);
+    document.querySelector ('#amigo').value = "";
+    atualizarlista();
+
+    }else {
+        alert('Por favor, insira um nome valido');
+    
+    }
+
+}
+    function sortearAmigo (){
+    
+        if (amigo.length > 0){// Verifica se o array não está vazio
+            let indiceAleatorio = Math.floor(Math.random() * amigo.length);// Gera um índice aleatório
+            let amigoSorteado = amigo[indiceAleatorio]; // Obtém o nome sorteado
+            console.log (`Amigo sorteado: ${amigoSorteado}`); // Exibe o nome sorteado no console
+        }
+        else {
+            alert('Não há amigos para sortear!')// Mensagem de alerta se o array estiver vazio
+        }
+    }
+function atualizarlista (){
+    let listaAmigos = document.querySelector ('#listaAmigos');
+    listaAmigos.innerHTML = "";
+
+    amigo.forEach (amigo =>{
+        let item = document.createElement('li');
+            item.textContent = amigo;
+            listaAmigos.appendChild(item);
+        
+    });
 }
